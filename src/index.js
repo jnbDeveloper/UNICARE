6b49203e2d1b535cb072b4a485b6d763c2be5d09
kasun-buddhika <buddhikakasun329@gmail.com>
2023-11-23 20:09:34 +0530

    Changed the login page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Login from "./screens/Login";
+import Login from "./pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
